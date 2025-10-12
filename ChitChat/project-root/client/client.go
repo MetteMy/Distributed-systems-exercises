f7d1f30b7ea5d9f5431c9871d8ebc673494c1782
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-10-12 11:03:03 +0200

    Added test exchange between service and client

--- ChitChat/project-root/client/client.go
+++ ChitChat/project-root/client/client.go
@@ -1,8 +1,30 @@
-package client
+package main
 
 import (
 	"context"
+	"log"
+	pb "project-root/grpc"
+
 	"google.golang.org/grpc"
 )
 
-type client struct{}
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("localhost:9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewChitChatServiceClient(conn)
+
+	message := pb.Message{
+		Body: "Hello from the client:)",
+	}
+
+	response, err := c.SayHello(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", response.Body)
+}
--- ChitChat/project-root/server/server.go
+++ ChitChat/project-root/server/server.go
@@ -1,8 +1,38 @@
-package server
+package main
 
 import (
 	"context"
+	"log"
+	"net"
+	pb "project-root/grpc"
+
 	"google.golang.org/grpc"
 )
 
-type server struct{}
+type server struct {
+	pb.UnimplementedChitChatServiceServer
+}
+
+func main() {
+	//instantiate listener
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("server listening at %v", lis.Addr())
+
+	//server instance:
+	grpcServer := grpc.NewServer()
+	pb.RegisterChitChatServiceServer(grpcServer, &server{})
+
+	//listen and serve
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func (s *server) SayHello(ctx context.Context, message *pb.Message) (*pb.Message, error) {
+	log.Printf("Received: %v", message.Body)
+	return &pb.Message{Body: "Hello from the server"}, nil
+}
