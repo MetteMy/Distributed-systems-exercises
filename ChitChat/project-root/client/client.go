84eea3fb17e9ba393cc1ec59f0b48d4e513c5428
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-10-22 11:14:08 +0200

    Changed to transportCredentials

--- ChitChat/project-root/client/client.go
+++ ChitChat/project-root/client/client.go
@@ -11,11 +11,12 @@ import (
 	"syscall"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial("0.0.0.0:9000", grpc.WithInsecure())
+	conn, err := grpc.Dial("0.0.0.0:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
